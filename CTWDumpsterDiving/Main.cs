ce9bf82a7660746e7cbd8a5e5f8380599d029bed
Hannele Ruiz <justlemoncl@gmail.com>
2018-08-15 21:13:19 -0300

    Added basic tick event + setup

--- CTWDumpsterDiving/Main.cs
+++ CTWDumpsterDiving/Main.cs
@@ -1,5 +1,21 @@
 using GTA;
+using System;
 
 public class DumpsterDiving : Script
 {
+
+    public DumpsterDiving()
+    {
+        // Add our events
+        Tick += OnTick;
+
+        // Just an example message
+        // TODO: Print the version and type of build
+        UI.Notify("DumpsterDiving has been loaded!");
+    }
+
+    private void OnTick(object Sender, EventArgs Args)
+    {
+
+    }
 }
