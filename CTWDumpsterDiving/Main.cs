40d9eb18cc79398c77179a6f95988e69b76b7353
Hannele Ruiz <justlemoncl@gmail.com>
2018-08-15 22:05:16 -0300

    Create temporary blips for the dumpsters near us

--- CTWDumpsterDiving/Main.cs
+++ CTWDumpsterDiving/Main.cs
@@ -13,9 +13,9 @@ public class DumpsterDiving : Script
         new Model("prop_dumpster_04a")
     };
     /// <summary>
-    /// A list with the map blips that we have created.
+    /// The dumpsters that should be near us.
     /// </summary>
-    public static List<Blip> Blips = new List<Blip>();
+    public static List<Prop> NearDumpsters = new List<Prop>();
 
     public DumpsterDiving()
     {
@@ -38,8 +38,21 @@ public class DumpsterDiving : Script
                 // Check that the Prop is visible and is near 15 units to the player
                 if (CurrentProp.IsVisible && World.GetDistance(Game.Player.Character.Position, CurrentProp.Position) <= 15f)
                 {
-                    // TODO: Do something
+                    Blip PropBlip = CurrentProp.AddBlip();
+                    PropBlip.Name = "Dumpster";
+                    PropBlip.Sprite = BlipSprite.Devin;
+                    PropBlip.Color = BlipColor.Green;
                 }
+            }
+        }
+
+        // Iterate over the recent dumpsters
+        foreach (Prop Recent in NearDumpsters)
+        {
+            // If the dumpster is far and has a blip attached
+            if (World.GetDistance(Game.Player.Character.Position, Recent.Position) > 15f && Recent.CurrentBlip != null)
+            {
+                Recent.CurrentBlip.Remove();
             }
         }
     }
