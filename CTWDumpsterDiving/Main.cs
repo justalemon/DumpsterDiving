4e2befc5843deda1c1d2fb6a5db535bc4146e2b1
Hannele Ruiz <justlemoncl@gmail.com>
2018-08-16 01:37:46 -0300

    Added configuration option to enable and disable blips

--- CTWDumpsterDiving/Main.cs
+++ CTWDumpsterDiving/Main.cs
@@ -13,6 +13,10 @@ public class DumpsterDiving : Script
         new Model("prop_dumpster_02b"),
         new Model("prop_dumpster_04a")
     };
+    /// <summary>
+    /// The configuration for our current script.
+    /// </summary>
+    public static ScriptSettings ScriptConfig = ScriptSettings.Load("scripts\\DumpsterDiving.ini");
 
     public DumpsterDiving()
     {
@@ -27,27 +31,31 @@ public class DumpsterDiving : Script
 
     private void OnTick(object Sender, EventArgs Args)
     {
-        // Iterate over our Dumpster models
-        foreach (Model PropModel in Dumpsters)
+        // If the user wants blips, work on it
+        if (ScriptConfig.GetValue("CWDD", "Blips", false))
         {
-            // Iterate over the props for the model
-            foreach (Prop CurrentProp in World.GetAllProps(PropModel))
+            // Iterate over our Dumpster models
+            foreach (Model PropModel in Dumpsters)
             {
-                // Get the distance in units between the player and the Dumpster
-                float Distance = World.GetDistance(Game.Player.Character.Position, CurrentProp.Position);
-                // Check that the Prop is visible, is near 15 units to the player and it does not have a blip attached
-                if (CurrentProp.IsVisible && Distance <= 25f && !CurrentProp.CurrentBlip.Exists())
+                // Iterate over the props for the model
+                foreach (Prop CurrentProp in World.GetAllProps(PropModel))
                 {
-                    Blip PropBlip = CurrentProp.AddBlip();
-                    PropBlip.Name = "Dumpster";
-                    PropBlip.Sprite = BlipSprite.Devin;
-                    PropBlip.Color = BlipColor.Green;
-                }
-                // If the dumpster is far and has a blip attached, remove it
-                if (Distance > 25f && CurrentProp.CurrentBlip.Exists())
-                {
-                    UI.Notify("Deleting Prop" + CurrentProp.GetHashCode().ToString());
-                    CurrentProp.CurrentBlip.Remove();
+                    // Get the distance in units between the player and the Dumpster
+                    float Distance = World.GetDistance(Game.Player.Character.Position, CurrentProp.Position);
+                    // Check that the Prop is visible, is near 15 units to the player and it does not have a blip attached
+                    if (CurrentProp.IsVisible && Distance <= 25f && !CurrentProp.CurrentBlip.Exists())
+                    {
+                        Blip PropBlip = CurrentProp.AddBlip();
+                        PropBlip.Name = "Dumpster";
+                        PropBlip.Sprite = BlipSprite.Devin;
+                        PropBlip.Color = BlipColor.Green;
+                    }
+                    // If the dumpster is far and has a blip attached, remove it
+                    if (Distance > 25f && CurrentProp.CurrentBlip.Exists())
+                    {
+                        UI.Notify("Deleting Prop" + CurrentProp.GetHashCode().ToString());
+                        CurrentProp.CurrentBlip.Remove();
+                    }
                 }
             }
         }
