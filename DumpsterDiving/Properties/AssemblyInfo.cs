20c5f39cf3f2f58ce4c3e96c9968ec7710297e99
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-18 04:09:10 -0400

    Updating the copyright and version information

--- DumpsterDiving/Properties/AssemblyInfo.cs
+++ DumpsterDiving/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Dumpster Diving from GTA: Chinatown Wars in Grand Theft Auto V")]
 [assembly: AssemblyCompany("justalemon")]
 [assembly: AssemblyProduct("DumpsterDiving")]
-[assembly: AssemblyCopyright("© 2018 Hannele 'Lemon' Ruiz & Derpy-Canadian")]
+[assembly: AssemblyCopyright("© 2018-2019 Hannele 'Lemon' Ruiz & Starman")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("845b596a-fee8-451b-baae-0b385b861a0d")]
 
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
