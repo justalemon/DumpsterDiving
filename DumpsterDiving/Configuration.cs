c695f2a1e8a2105bac204a62ac564e6aeeb24ea9
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-18 04:22:54 -0400

    Added what every configuration parameter does

--- DumpsterDiving/Configuration.cs
+++ DumpsterDiving/Configuration.cs
@@ -4,14 +4,29 @@ namespace DumpsterDiving
 {
     public class Configuration
     {
+        /// <summary>
+        /// If the nearby dumpsters should have blips on the minimap.
+        /// </summary>
         [JsonProperty("blips")]
         public bool Blips { get; set; }
+        /// <summary>
+        /// If a sound should be played when the dumpster is being looted.
+        /// </summary>
         [JsonProperty("sound")]
         public bool Sound { get; set; }
+        /// <summary>
+        /// If the screen should fade during the looting process.
+        /// </summary>
         [JsonProperty("fade")]
         public int Fade { get; set; }
+        /// <summary>
+        /// The minimum distance from the player to the dumpster to show a marker.
+        /// </summary>
         [JsonProperty("markerdistance")]
         public float MarkerDistance { get; set; }
+        /// <summary>
+        /// The minimum distance from the player to the dumpster to start the looting process.
+        /// </summary>
         [JsonProperty("lootdistance")]
         public float LootDistance { get; set; }
     }
